1549f9dbe0dbdcdb150085e15392b4625f53ecee
MJ Habib <mj.habib4@gmail.com>
2024-01-16 19:55:24 +0330

    show the issues

--- src/app/components/Spinner.tsx
+++ src/app/components/Spinner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Spinner() {
   return (
     <div
-      className='inline-block h-4 w-4 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] text-neutral-100 opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]'
+      className='inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
       role='status'
     >
       <span className='!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]'>
