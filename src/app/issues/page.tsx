3452e692c48fa2b0ef6266b1822a01e0cbc5ef6a
MJ Habib <mj.habib4@gmail.com>
2024-01-16 10:50:10 +0330

    setup radix ui

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Button } from '@radix-ui/themes';
 
 export default function IssuesPage() {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 }
