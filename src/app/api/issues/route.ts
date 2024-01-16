c1bfcc49667b60aeb5ede048727bbd6c4793b030
MJ Habib <mj.habib4@gmail.com>
2024-01-16 13:58:22 +0330

    handle errors

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -3,8 +3,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import { z } from 'zod';
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, 'Title is Required').max(255),
+  description: z.string().min(1, 'Description is Required'),
 });
 
 export async function POST(request: NextRequest) {
@@ -12,7 +12,7 @@ export async function POST(request: NextRequest) {
   const validate = createIssueSchema.safeParse(body);
 
   if (!validate.success) {
-    return NextResponse.json(validate.error.errors);
+    return NextResponse.json(validate.error.format());
   }
 
   // const checkIssue = await prisma.issue.findUnique({
