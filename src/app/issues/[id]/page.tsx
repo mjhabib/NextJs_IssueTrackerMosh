f1e9751ed9607b7c70bd7359473b6ee3c823be64
MJ Habib <mj.habib4@gmail.com>
2024-01-17 14:40:17 +0330

    add markdown preview

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -2,15 +2,13 @@ import prisma from '@/root/prisma/client';
 import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import IssueStatusBadges from '../../components/IssueStatusBadges';
+import ReactMarkdown from 'react-markdown';
 
 interface Props {
   params: { id: string };
 }
 
 export default async function IssueDetailPage({ params }: Props) {
-  if (typeof params.id !== 'number') notFound();
-  // to prevent crashing the app in case of user type any path besides type int into the address bar, because the 'parseInt' function below can't parse other params like string
-
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -27,8 +25,8 @@ export default async function IssueDetailPage({ params }: Props) {
         <IssueStatusBadges status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        <p>{issue.description}</p>
+      <Card className='prose' mt='4'>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   );
