14d932758854569128286319782ddb080b949727
MJ Habib <mj.habib4@gmail.com>
2024-01-20 11:54:20 +0330

    finish part07, Authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import NavBar from './NavBar';
 import { Container, Theme, ThemePanel } from '@radix-ui/themes';
-import AuthProvider from '@src/auth/provider';
+import AuthProvider from '@src/auth/Provider';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
